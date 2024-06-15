f4fa71a8bd98ba4cf0cc1280e8b096c62df9ceed
Nicolas Martin <nicolasfelipemartin@gmail.com>
2024-06-15 13:01:34 -0500

    deploy studendt with addStudent

--- ignition/modules/student.deploy.ts
+++ ignition/modules/student.deploy.ts
@@ -3,6 +3,9 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 const studentModule = buildModule('Student', (module) => {
     const student = module.contract('StudentContract');
 
-    module.call(student, 'addStudent', []);
+    const newStudent = module.call(student, "addStudent", ["CC","1023959749","Nicolas","16-04-1997", "nicolasfelipemartin@gmail.com", "3002367244","calle 78C #110-16"]);
+    console.log(newStudent);
     return {student};
 });
+
+export default studentModule;
